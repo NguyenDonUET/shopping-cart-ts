f42b290b4a76572daf8244b095647fe4d53f8c9a
NguyenDonUET <nguyendoncb@gmail.com>
2023-10-09 23:49:48 +0700

    load data and add icon

--- src/features/product/productSlice.ts
+++ src/features/product/productSlice.ts
@@ -8,6 +8,33 @@ export type ProductType = {
   des: string;
 };
 
+export const products: ProductType[] = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1524592094714-0f0654e20314?crop=entropy&cs=srgb&fm=jpg&ixid=M3w0OTc0NTF8MHwxfHNlYXJjaHw1fHx3YXRjaHxlbnwwfHx8fDE2OTY3NzY3NTZ8MA&ixlib=rb-4.0.3&q=85",
+    id: 1,
+    name: "Widget",
+    price: 9.99,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ratione eum magnam?",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?crop=entropy&cs=srgb&fm=jpg&ixid=M3w0OTc0NTF8MHwxfHNlYXJjaHwyfHxwaG9uZXxlbnwwfHx8fDE2OTY3NzY4MTB8MA&ixlib=rb-4.0.3&q=85",
+    id: 2,
+    name: "Premium Widget",
+    price: 19.99,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ratione eum magnam?",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1565849904461-04a58ad377e0?crop=entropy&cs=srgb&fm=jpg&ixid=M3w0OTc0NTF8MHwxfHNlYXJjaHw3fHxwaG9uZXxlbnwwfHx8fDE2OTY3NzY4MTB8MA&ixlib=rb-4.0.3&q=85",
+    id: 3,
+    name: "Deluxe Widget",
+    price: 29.99,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ratione eum magnam?",
+  },
+];
+
 export type ProductState = {
   products: ProductType[];
   loading: boolean;
@@ -15,13 +42,13 @@ export type ProductState = {
 };
 
 const initialState: ProductState = {
-  products: [],
+  products: products || [],
   loading: true,
   error: false,
 };
 
 export const getProducts = createAsyncThunk("products/getAll", async () => {
-  const response = await fetch("http://localhost:5000/products");
+  const response = await fetch("products.json");
   return (await response.json()) as ProductType[];
 });
 
